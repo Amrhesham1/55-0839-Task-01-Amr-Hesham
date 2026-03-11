cd29c4fa9656b79b33e2d8782d0b45917820bf45
Amr Hesham Aboulfotouh <161937162+Amrhesham1@users.noreply.github.com>
2026-03-11 21:44:29 +0200

    project setup

---
+++ src/main/java/s55_0839/t_20/Amr_Hesham/AmrHeshamApplication.java
@@ -0,0 +1,13 @@
+package s55_0839.t_20.Amr_Hesham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmrHeshamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmrHeshamApplication.class, args);
+	}
+
+}
