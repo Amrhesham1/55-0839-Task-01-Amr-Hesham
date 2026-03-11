1b625cd3cbfc17973973e9d16eef39cc0163fb0c
Amr Hesham Aboulfotouh <161937162+Amrhesham1@users.noreply.github.com>
2026-03-11 21:44:29 +0200

    Initial commit: project setup

---
+++ src/test/java/s55_0839/t_20/Amr_Hesham/AmrHeshamApplicationTests.java
@@ -0,0 +1,13 @@
+package s55_0839.t_20.Amr_Hesham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmrHeshamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
